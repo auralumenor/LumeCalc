8e75cf67f1b758fed4a751737b01465f4c6796d0
Raman Raj <rr737902@gmail.com>
2026-01-05 17:44:29 +0530

    Fixed

---
+++ build/CMakeFiles/LumeCalc.dir/calc.c.obj.d
@@ -0,0 +1,19 @@
+CMakeFiles/LumeCalc.dir/calc.c.obj: \
+ E:\BTech_AI_ML\Semester_1\Other\ _works\LumeCalc\calc.c \
+ C:/MinGW64/x86_64-w64-mingw32/include/stdio.h \
+ C:/MinGW64/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/MinGW64/x86_64-w64-mingw32/include/corecrt.h \
+ C:/MinGW64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/MinGW64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/MinGW64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/MinGW64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/MinGW64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/MinGW64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/MinGW64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/MinGW64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/MinGW64/x86_64-w64-mingw32/include/math.h \
+ C:/MinGW64/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/MinGW64/x86_64-w64-mingw32/include/conio.h \
+ C:/MinGW64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stddef.h \
+ C:/MinGW64/x86_64-w64-mingw32/include/stddef.h \
+ C:/MinGW64/x86_64-w64-mingw32/include/sec_api/conio_s.h
---
+++ build/CMakeFiles/LumeCalc.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for LumeCalc.
+# This may be replaced when dependencies are built.
